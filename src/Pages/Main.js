000466c4e39ddf3f911cb4812b4db7068bb9b604
saira bunny espino <espinosairabunny@gmail.com>
2019-12-04 21:28:22 +0800

    ade changes to backend

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -3,17 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Main = () => {
     return(
        <Fragment>
-           {/* <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
-               <div className="carousel-inner">
-                <div class="carousel-item active">
-                    <img className="d-block w-100" src="https://images.pexels.com/photos/1190298/pexels-photo-1190298.jpeg?" alt="First slide" />
-                </div>
-               <div className="carousel-item">
-                    <img className="d-block w-100" src="https://images.pexels.com/photos/2337777/pexels-photo-2337777.jpeg?" alt="Second slide" />
-               </div>
-            </div>
-           </div> */}
-
+         
+{/* 
            <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
@@ -26,7 +17,7 @@ const Main = () => {
                 <img class="d-block w-100" src="..." alt="Third slide" />
                 </div>
             </div>
-            </div>
+            </div> */}
        </Fragment>
     )
 }
